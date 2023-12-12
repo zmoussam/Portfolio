20a82edb9ee75d89caf61c4f472d022309779ab4
zmoussam <zakariamoussammi87@gmail.com>
2023-12-12 03:17:51 +0100

    Tue Dec 12 03:17:51 +01 2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,15 +29,80 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blurHeader class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_4dtawex', 'template_banhp65', '#contact-form', 'FmNcdd6bUZhhTBYtE')
+            .then(() => {
+                contactMessage.textContent = 'Message sent successfully!';
+
+                // remove message after 5 seconds
+                setTimeout(() => contactMessage.textContent = '', 5000);
+
+                // clear input fields
+                contactForm.reset();
+            }, () => {
+                // show error message 
+                contactMessage.textContent = 'Message failed to send!!';
+            });
+}
+    
+contactForm.addEventListener('submit', sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 400,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true // reset animation repeat
+})
+
+sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`);
+sr.reveal(`.home__image`, {origin: 'bottom'});
+sr.reveal(`.about__data, .skills__data`, {origin: 'left'});
+sr.reveal(`.about__image, .skills__content`, {origin: 'right'});
+sr.reveal(`.services__card, .projects__card`, {interval: 100});
